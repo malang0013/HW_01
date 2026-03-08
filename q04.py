663413eaaa5cda79c2b2a0fcc31be9d982411c50
malang0013 <gm050108@gmail.com>
2026-03-08 23:39:47 +0900

    Add: Q04 solution

---
+++ q04.py
@@ -0,0 +1,32 @@
+Nk= input().split()
+
+N = int(Nk[0])
+k = int(Nk[1])
+
+arr1 = list(map(int,input().split()))
+arr2 = list(map(int,input().split()))
+
+for i in range(N):
+    min = i
+    for j in range(i+1, N):
+        if arr1[j] < arr1[min]:
+            min = j
+            arr1[i], arr1[min] = arr1[min], arr1[i]
+
+
+for i in range(N):
+    max = i
+    for j in range(i+1, N):
+        if arr2[j] > arr2[max]:
+            max = j
+            arr2[i], arr2[max] = arr2[max], arr2[i]
+
+
+for i in range(k):
+    if (arr1[i] < arr2[i]):
+        arr1[i], arr2[i] = arr2[i], arr1[i]
+
+    else:
+        break
+
+print(sum(arr1))
